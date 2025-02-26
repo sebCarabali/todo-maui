0890bac90e98262ee5cd1fd15376633b2350d855
Sebastián Carabali <secarabali@gmail.com>
2025-02-26 11:33:28 -0500

    Some commit after vm reaching view

---
+++ LoginApplication/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApplication.ViewModels
+{
+    [ObservableObject]
+    public partial class LoginViewModel
+    {
+        [ObservableProperty]
+        private string userName;
+        [ObservableProperty]
+        private string password;
+
+        [RelayCommand]        
+        private void Login()
+        {
+            // TODO: Call the authentication service and save the jwt(JSON Web Token) token 
+            Console.WriteLine($"Try to login with {userName} and {password}");
+        }
+    }
+}
---
+++ LoginApplication/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using LoginApplication.ViewModels;
+
+namespace LoginApplication.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private LoginViewModel vm = new LoginViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+    }
+
+}
