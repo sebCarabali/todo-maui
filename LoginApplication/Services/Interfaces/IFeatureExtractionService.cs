2f187bee3ad2885b66cfba5658b146f6f2fc8269
Jose Luis Guaca <jlguacac@gmail.com>
2025-03-11 15:28:54 -0500

    Fin logout inicial

---
+++ LoginApplication/Services/Interfaces/IFeatureExtractionService.cs
@@ -0,0 +1,14 @@
+﻿using LoginApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApplication.Services.Interfaces
+{
+    public interface IFeatureExtractionService
+    {
+        Task<FeatureVector> ExtractFeaturesAsync(Stream compressedImage);
+    }
+}
